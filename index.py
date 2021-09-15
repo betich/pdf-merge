396ebec3c48d52e987238a75810ecc9f32e51d9d
betich <betich365@gmail.com>
2021-09-15 10:06:57 +0700

    :art: remove comment

--- index.py
+++ index.py
@@ -12,7 +12,6 @@ def pdf_merge():
     merger = PdfFileMerger()
     allpdfs = [a for a in glob("src/*.pdf")]
     sortedpdfs = sorted(allpdfs, key=first2)
-    print(sortedpdfs)
     [merger.append(pdf) for pdf in sortedpdfs]
     with open("output.pdf", "wb") as new_file:
         merger.write(new_file)
