4d162f3d06d0c661a4179a9e94651800fb18d29a
betich <betich365@gmail.com>
2021-09-15 09:56:59 +0700

    :wave: initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# pdf merge
+
+a script used to merge all pdf files into one (used for collecting all my classmate's files and combining into one final output)
+
+tl;dr i'm lazy
+
+## setup
+
+1. install python
+
+2. install PyPDF2
+
+```
+pip3 install PyPDF2
+```
+
+3. put all pdf files into `src`
+
+everything is sorted numerically, so:
+
+for front covers, name it something low like `-99.pdf`
+
+for back covers, name it something high like: `99.pdf`
+
+4. run the code
+
+```
+python3 index.py
+```
+
+1. have fun!
---
+++ index.py
@@ -0,0 +1,23 @@
+from glob import glob
+from PyPDF2 import PdfFileMerger
+from os import path
+
+def first2(x):
+    # src/00.pdf
+    print(path.basename(x))
+    return(path.basename(x))
+
+def pdf_merge():
+    ''' Merges all the pdf files in current directory '''
+    merger = PdfFileMerger()
+    allpdfs = [a for a in glob("src/*.pdf")]
+    sortedpdfs = sorted(allpdfs, key=first2)
+    print(sortedpdfs)
+    [merger.append(pdf) for pdf in sortedpdfs]
+    with open("output.pdf", "wb") as new_file:
+        merger.write(new_file)
+
+
+if __name__ == "__main__":
+    pdf_merge()
+    print("done")
